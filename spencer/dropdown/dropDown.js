9186b716ee5b5acf0bbbe7e5ca4cae324b0486e4
Spencer McWilliams <spencer.mcw@gmail.com>
2014-09-10 11:54:18 -0700

    adds: /spencer custom directive and json pulling for dropdown.

--- spencer/dropdown/dropDown.js
+++ spencer/dropdown/dropDown.js
@@ -1,12 +1,18 @@
 var app = angular.module('app', []);
 
+app.controller('dropdownController', ['$http', function($http){
+    var dropdown = this;
+    dropdown.states = [];
+    $http.get('https://gist.githubusercontent.com/mshafrir/2646763/raw/bfb35f17bc5d5f86ec0f10b80f7b80e823e9197f/states_titlecase.json').success(function(data){
+        dropdown.states = data;
+    });
+}]);
+
 app.directive('dropDown', function () {
     return {
         restrict: 'AE',
         templateUrl: 'drop-down.html',
-        controller:function(){
-            this.values = ["Internet Explored", "Google Chrone"];
-        },
+        controller: 'dropdownController',
         controllerAs: 'dropdownCtrl'
     };
 });
