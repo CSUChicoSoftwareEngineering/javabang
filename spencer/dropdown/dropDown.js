b9d4d1e745ad44bfa246ab5a9921109550259c74
Spencer McWilliams <spencer.mcw@gmail.com>
2014-09-15 10:40:39 -0700

    adds: finished spencer dropdown

--- spencer/dropdown/dropDown.js
+++ spencer/dropdown/dropDown.js
@@ -3,14 +3,15 @@ var app = angular.module('app', []);
 app.controller('dropdownController', ['$http', function($http){
     var dropdown = this;
     dropdown.states = [];
-    $http.get('https://gist.githubusercontent.com/mshafrir/2646763/raw/bfb35f17bc5d5f86ec0f10b80f7b80e823e9197f/states_titlecase.json').success(function(data){
+    // $http.get('https://gist.githubusercontent.com/mshafrir/2646763/raw/bfb35f17bc5d5f86ec0f10b80f7b80e823e9197f/states_titlecase.json').success(function(data){
+    $http.get('/states.json').success(function(data){
         dropdown.states = data;
     });
 }]);
 
 app.directive('dropDown', function () {
     return {
-        restrict: 'AE',
+        restrict: 'E',
         templateUrl: 'drop-down.html',
         controller: 'dropdownController',
         controllerAs: 'dropdownCtrl'
