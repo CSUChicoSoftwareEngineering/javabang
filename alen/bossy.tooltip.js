0ae96f2e79bd52b1fa8269f4bc987f56619b5a5c
AlmondSeason <almondseason@gmail.com>
2014-10-01 00:22:21 -0700

    tooltip progress
    Updates tooltip; still unfinished

---
+++ alen/bossy.tooltip.js
@@ -0,0 +1,46 @@
+ angular.module('bossy.Tooltip', [])
+	.run(function($templateCache) {
+		$templateCache.put('jasmineTest.html', 'jasmineTest.html');
+	})
+	
+	.factory('bossyTooltipFactory', function() {
+		return {
+			_options: {
+				title: 'factory title',
+				content: 'factory content'
+				}
+		}
+	})
+	
+	.directive('bossyTooltip', function($compile, $http /*$data,*/ /*$schema*/) {
+		return {
+			restrict: 'EA',
+			replace: true,
+			template: ' <div> Title: {{tips._options.title}} Content: {{tips._options.content}} </div>' ,
+			//templateUrl: 'bossytool.html',
+			controller:['$scope', 'bossyTooltipFactory', function($scope, bossyTooltipFactory) {
+					this._options = {
+					title: bossyTooltipFactory._options.title,
+					content: bossyTooltipFactory._options.content
+				};
+			}],
+			controllerAs: "tips"		
+		};
+	})
+	
+	.directive('onEnter', function() {
+		return function(scope, element) {
+			element.bind('mouseenter', function() {
+				console.log("I'm in you");
+			})
+		}
+	})
+	
+	.directive('onExit', function() {
+		return function(scope, element) {
+			element.bind('mouseleave', function() {
+				console.log("I'm outta here");
+			})
+		}
+	})
+	
